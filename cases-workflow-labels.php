86d8fdff512ce8567e4b14d6d0dcf66027bc9928
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2012-11-05 01:27:55 +0400

    Added filtering labels by person

--- cases-workflow-labels.php
+++ cases-workflow-labels.php
@@ -5,7 +5,7 @@ Plugin URI: http://wpcases.com/
 Description: Ярлыки для сортировки и обработки дел по стандарту документооборота.
 Author: Sergey Biryukov, Ivan Vinogradov
 Author URI: http://profiles.wordpress.org/sergeybiryukov/
-Version: 0.3.1
+Version: 0.3.2
 */ 
 
 function cwl_register_taxonomy() {
@@ -43,14 +43,14 @@ add_action( 'init', 'cwl_register_taxonomy' );
 function cwl_add_labels_to_new_cases( $meta_id, $object_id, $meta_key, $meta_value ) {
 	switch ( $meta_key ) {
 		case 'initiator' :
-			$label_name = $meta_value . '_Исходящие';
+			$label_name = $meta_value . '-Исходящие';
 			wp_set_post_terms( $object_id, $label_name, 'labels', true );
 			break;
 		case 'responsible' :
 		case 'participant' :
 			$user_ids = explode( ',', $meta_value );
 			foreach ( (array) $user_ids as $user_id ) {
-				$label_name = $user_id . '_Входящие';
+				$label_name = $user_id . '-Входящие';
 				wp_set_post_terms( $object_id, $label_name, 'labels', true );
 			}
 			break;
@@ -140,7 +140,7 @@ function cwl_ajax_add_labels() {
 		wp_insert_term( $_POST['labels'], 'labels' );
 	} else {
 		foreach ( (array) $_POST['posts'] as $post_id )
-			wp_set_object_terms( $post_id, $_POST['labels'], 'labels' );
+			wp_set_post_terms( $post_id, $_POST['labels'], 'labels', true );
 	}
 
 	cwl_get_label_list();
@@ -148,4 +148,96 @@ function cwl_ajax_add_labels() {
 	die();
 }
 add_action( 'wp_ajax_add_labels', 'cwl_ajax_add_labels' );
+
+function cwl_get_person_id_by_email( $email ) {
+
+	$person = new WP_Query( array(
+		'fields' => 'ids',
+		'post_type' => 'persons',
+		'meta_query' => array( array( 'key' => 'email', 'value' => $email ) ),
+	) );
+
+	return array_shift( $person->posts );
+}
+
+function cwl_filter_labels_by_person( $args, $taxonomies ) {
+	if ( ! is_user_logged_in() || array( 'labels' ) !== $taxonomies )
+		return $args;
+
+	$person_id = cwl_get_person_id_by_email( wp_get_current_user()->user_email );
+
+	if ( empty( $args['name__like'] ) )
+		$args['name__like'] = $person_id . '-';
+
+	return $args;
+}
+add_filter( 'get_terms_args', 'cwl_filter_labels_by_person', 10, 2 );
+
+function cwl_hide_prefix_from_term_names( $terms, $taxonomies, $args ) {
+	if ( ! is_user_logged_in() || array( 'labels' ) !== $taxonomies )
+		return $terms;
+
+	if ( ! is_array( $terms ) )
+		return $terms;
+
+	$person_id = cwl_get_person_id_by_email( wp_get_current_user()->user_email );
+
+	foreach ( $terms as $key => $term )
+		$terms[ $key ]->name = str_replace( $person_id . '-', '', $term->name );
+
+	return $terms;
+}
+add_filter( 'get_terms', 'cwl_hide_prefix_from_term_names', 10, 3 );
+
+function cwl_remove_from_inbox() {
+	global $post;
+
+	if ( ! isset( $_GET['action'] ) || 'remove-from-inbox' != $_GET['action'] )
+		return;
+
+	if ( ! is_user_logged_in() || ! is_singular( 'cases' ) )
+		return;
+
+	$person_id = cwl_get_person_id_by_email( wp_get_current_user()->user_email );
+
+	$terms = wp_get_post_terms( $post->ID, 'labels', array( 'fields' => 'names' ) );
+	$terms = array_diff( $terms, array( $person_id . '-Входящие' ) );
+
+	wp_set_post_terms( $post->ID, $terms, 'labels' );
+
+	wp_redirect( get_permalink() );
+	die();
+}
+add_action( 'wp', 'cwl_remove_from_inbox' );
+
+function cwl_echo_remove_from_inbox_link() {
+	global $post;
+
+	if ( 'cases' != $post->post_type )
+		return;
+
+	$person_id = cwl_get_person_id_by_email( wp_get_current_user()->user_email );
+	if ( ! has_term( sanitize_title( $person_id . '-Входящие' ), 'labels', $post->ID ) )
+		return;
+
+	$url = add_query_arg( array( 'action' => 'remove-from-inbox' ), get_permalink() );
+
+	echo sprintf( '<a class="btn btn-mini" href="%s">Убрать из Входящих</a>', $url );
+}
+add_action( 'roots_entry_meta_before', 'cwl_echo_remove_from_inbox_link' );
+
+/*
+function cwl_convert_labels() {
+	remove_filter( 'get_terms', 'cwl_hide_prefix_from_term_names', 10, 3 );
+	$terms = get_terms( 'labels', 'hide_empty=0' );
+
+	foreach ( $terms as $key => $term ) {
+		$term->name = str_replace( '_', '-', $term->name );
+		$term->slug = str_replace( '_', '-', $term->slug );
+		wp_update_term( $term->term_id, 'labels', array( 'name' => $term->name, 'slug' => $term->slug ) );
+	}
+
+}
+// add_action( 'init', 'cwl_convert_labels' );
+*/
 ?>
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: SergeyBiryukov, IvanVinogradov
 Tags: cases, workflow, labels
 Requires at least: 3.0
 Tested up to: 3.4
-Stable tag: 0.3.1
+Stable tag: 0.3.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -19,6 +19,10 @@ Adds workflow labels to ACM Cases.
 2. Activate the plugin through the 'Plugins' menu in WordPress.
 
 == Changelog ==
+
+= 0.3.2 =
+* Added filtering labels by person
+* Added "Remove from Inbox" button
 
 = 0.3.1 =
 * Added basic functionality to label management box
